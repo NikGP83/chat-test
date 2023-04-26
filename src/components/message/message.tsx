c21fc83dc95e0793869e2fd79c7d0947b6e7d812
Nik G <nik.golovin83@gmail.com>
2023-04-26 17:39:58 +0600

    добавил search в сайдбар, добавил стили в header, добавляю стили в сообщения

--- src/components/message/message.tsx
+++ src/components/message/message.tsx
@@ -4,35 +4,31 @@ import './message.scss';
 import { ChatMessage } from '../../types/types';
 import { getTime } from '../../utils/utils';
 
-
-
-function Message({user, created_at, message}: ChatMessage) {
-
-  const {name, surname, you} = user;
+function Message({ user, created_at, message }: ChatMessage) {
+  const { name, surname, you } = user;
   return (
     <div className='message-wrapper'>
       <div>
-
-      {!you && (
-        <>
-          <div className='message-avatar'>
-            <Avatar {...user} />
-          </div>
-          <div className='users-block'>
-            <span className='user-name'>{name}</span>
-            <span className='user-surname'>{surname}</span>
-          </div>
-        </>
-      )}
-      <div className='message-content'>
-        <div className={`message-bubble ${you && `your-message`}`}>
-          <p className='message-text'>{message}</p>
-          <div className='message-time'>{getTime(created_at)}</div>
-          {you && (
-            <div className='message-readed'>
-              <img src={readedIcon} alt='icon readed' />
+        {!you && (
+          <>
+            <div className='users-block'>
+              <div className='message-avatar'>
+                <Avatar {...user} size='sm'/>
+              </div>
+              <span className='user-name'>{name}</span>
+              <span className='user-surname'>{surname}</span>
             </div>
-          )}
+          </>
+        )}
+        <div className='message-content'>
+          <div className={`message-bubble ${you && `your-message`}`}>
+            <p className='message-text'>{message}</p>
+            <div className='message-time'>{getTime(created_at)}</div>
+            {you && (
+              <div className='message-readed'>
+                <img src={readedIcon} alt='icon readed' />
+              </div>
+            )}
           </div>
         </div>
       </div>
